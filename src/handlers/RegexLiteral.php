5f8bbe5a212c8fd7f94b78670e7e8fc63c747483
Smuuf <premavansmuuf@gmail.com>
2017-07-16 11:45:11 +0200

    Implemented RegexLiteral.

---
+++ src/handlers/RegexLiteral.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Smuuf\Primi\Handlers;
+
+use \Smuuf\Primi\Structures\RegexValue;
+use \Smuuf\Primi\Context;
+
+class RegexLiteral extends \Smuuf\Primi\Object implements IHandler {
+
+	public static function handle(array $node, Context $context) {
+		return new RegexValue($node['text']);
+	}
+
+}
