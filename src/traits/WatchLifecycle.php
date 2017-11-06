c1ad3b7b43da500d21ec177260b3436d66507c0c
Smuuf <premavansmuuf@gmail.com>
2017-11-06 23:51:40 +0100

    Added documentation to WatchLifecycle trait. + Refactored a bit.

--- src/traits/WatchLifecycle.php
+++ src/traits/WatchLifecycle.php
@@ -8,6 +8,7 @@ namespace Smuuf\Primi;
  */
 abstract class WatchLifecycleContext {
 
+    public static $instanceCounter = 0;
     public static $stackCounter = 0;
     public static $alreadyVisualized = [];
     public static $stack = [];
@@ -19,20 +20,35 @@ abstract class WatchLifecycleContext {
  */
 trait WatchLifecycle {
 
+    /** @var int Every unique instance of "watched" class's number. */
+    private static $instanceCounter;
+
     public function __construct() {
 
+        // Assign a new, globally unique counter's number for this new object.
+        self::$instanceCounter = WatchLifecycleContext::$instanceCounter++;
+
+        // Build a completely unique hash for this object's instance.
         $hash = self::getHash($this);
+
+        // Add this object to global watch stack.
         $this->add($hash);
 
+        // Build a unique true color for this instance.
         $colorhash = self::truecolor($hash, $hash);
         $visual = $this->visualize();
+
+        // Visualize newly created object with a pretty sign.
         echo "+  $colorhash $visual\n";
 
 	}
 
 	public function __destruct() {
 
+        // Build visualisation string before removing this object from watched stack.
         $visual = $this->visualize();
+
+        // Remove this object from global watch stack.
         $hash = self::getHash($this);
         $this->remove($hash);
 
@@ -71,20 +87,33 @@ trait WatchLifecycle {
 
     }
 
-    private static function getHash($object) {
-        return substr(md5(get_class($object) . spl_object_hash($object)), 0, 8);
+    /**
+     * Return a completely unique hash for this object's instance.
+     * Uniqueness is based off a 1) global counter, 2) class name, 3) spl_object_hash.
+     *
+     * Now that I'm thinking about it, the "1)" should be enough, but, well...
+     */
+    private static function getHash($object): string {
+        return substr(md5(self::$instanceCounter . get_class($object) . spl_object_hash($object)), 0, 8);
     }
 
     private static function truecolor(string $hex, string $content) {
 
-        $r = hexdec(substr($hex, 0, 2)) + 32;
-        $g = hexdec(substr($hex, 2, 2)) + 32;
-        $b = hexdec(substr($hex, 4, 2)) + 32;
+        $r = self::hexnumpad(substr($hex, 0, 2) + 32);
+        $g = self::hexnumpad(substr($hex, 2, 2) + 32);
+        $b = self::hexnumpad(substr($hex, 4, 2) + 32);
 
         $out = sprintf("\x1b[38;2;%s;%s;%sm", $r, $g, $b);
         $out .= $content . "\033[0m"; // Reset colors.
         return $out;
 
+    }
+
+    /**
+     * Return int number as hex and ensure it's of length of 3, padded with zeroes.
+     */
+    private static function hexnumpad(int $n) {
+        return str_pad(hexdec($n), 3, "0", STR_PAD_LEFT);
     }
 
 }
