fc1b01b6fcfddf92a7ac0c154df6691a73feb99a
Smuuf <premavansmuuf@gmail.com>
2018-05-22 09:28:59 +0200

    Added if-equality microbenchmark + fixed the others.

---
+++ res/phpcb-benchmarks/if_equality/index.php
@@ -0,0 +1,52 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . "/../../../vendor/autoload.php";
+$bench = new \Smuuf\Phpcb\PhpBenchmark(new \Smuuf\Phpcb\SerialEngine);
+
+// Was the winner.
+$bench->addBench(function() {
+
+	$c = 0;
+	$z = null;
+
+	if (!$z) {
+		$c++;
+	}
+
+});
+
+$bench->addBench(function() {
+
+	$c = 0;
+	$z = null;
+
+	if ($z === null) {
+		$c++;
+	}
+
+});
+
+$bench->addBench(function() {
+
+	$c = 0;
+	$z = true;
+
+	if ($z) {
+		$c++;
+	}
+
+});
+
+$bench->addBench(function() {
+
+	$c = 0;
+	$z = true;
+
+	if ($z === true) {
+		$c++;
+	}
+
+});
+
+$bench->run(1e7);
