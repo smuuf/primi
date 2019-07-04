1316f2c035e37048e46ac601713f609e630bebe9
Smuuf <premavansmuuf@gmail.com>
2019-07-04 11:29:10 +0200

    New PSL string function 'join'. - Usage examples:   - ",".join([1, 2, 3]) returns "1,2,3".   - ",".join([1, 2, ['nested', 'stuff'], 4] returns "1,2,nested,stuff,4".

--- src/extensions/psl/StringExtension.php
+++ src/extensions/psl/StringExtension.php
@@ -180,4 +180,29 @@ class StringExtension extends Extension {
 
 	}
 
+	public static function string_join(
+		StringValue $self,
+		ArrayValue $array
+	): StringValue {
+
+		$prepared = \array_map(function(&$item) use ($self) {
+
+			Common::allowTypes($item, StringValue::class, NumberValue::class,
+				BoolValue::class, ArrayValue::class);
+
+			switch (true) {
+				case $item instanceof ArrayValue:
+					return self::string_join($self, $item)->value;
+				case $item instanceof BoolValue:
+					return $item->getStringValue();
+				default:
+					return $item->value;
+			}
+
+		}, $array->value);
+
+		return new StringValue(implode($self->value, $prepared));
+
+	}
+
 }
