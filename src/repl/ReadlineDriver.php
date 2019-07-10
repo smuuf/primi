7d3dc4f2187817f8b97384af3322283265b6097f
Smuuf <premavansmuuf@gmail.com>
2019-07-10 22:38:19 +0200

    Don't store text into readline history if it's same as the last one entered.

--- src/repl/ReadlineDriver.php
+++ src/repl/ReadlineDriver.php
@@ -4,12 +4,23 @@ namespace Smuuf\Primi;
 
 class Readline extends \Smuuf\Primi\StrictObject implements IReadlineDriver {
 
+	private $lastItem = '';
+
 	public function readline(string $prompt): string {
 		return readline($prompt);
 	}
 
 	public function readlineAddHistory(string $item): void {
+
+		// Avoid storing the same value again, if it's the same value
+		// as before.
+		if ($this->lastItem === $item) {
+			return;
+		}
+
 		readline_add_history($item);
+		$this->lastItem = $item;
+
 	}
 
 	public function readlineReadHistory(string $path): void {
