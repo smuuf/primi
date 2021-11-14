e542f49dfdd61b59f78fbc917479284f8bb90a78
Smuuf <premavansmuuf@gmail.com>
2021-11-14 17:56:30 +0100

    Unified logic for building Function objects from PHP object's methods.
    New helper class and its method `MethodExtractor::extractMethods()`
    which extracts all public and non-PHP-magic methods from an object
    into callables represented as FuncValue PHP objects (Function objects in
    Primi).

--- src/Extensions/TypeExtension.php
+++ src/Extensions/TypeExtension.php
@@ -2,11 +2,8 @@
 
 namespace Smuuf\Primi\Extensions;
 
-use \Smuuf\Primi\Values\FuncValue;
 use \Smuuf\Primi\Helpers\ValueFriends;
-use \Smuuf\Primi\Structures\FnContainer;
-
-use \Smuuf\DocBlockParser\Parser as DocBlockParser;
+use \Smuuf\Primi\Helpers\MethodExtractor;
 
 abstract class TypeExtension extends ValueFriends {
 
@@ -15,55 +12,7 @@ abstract class TypeExtension extends ValueFriends {
 	 * contents of the module.
 	 */
 	public static function execute(): array {
-
-		// Extract additional values via meta info.
-		return self::extract(new static);
-
-	}
-
-	private static function extract(self $ext): array {
-
-		$result = [];
-		$extRef = new \ReflectionClass($ext);
-
-		//
-		// Extract functions.
-		//
-
-		$methods = $extRef->getMethods(\ReflectionMethod::IS_PUBLIC);
-		foreach ($methods as $ref) {
-
-			$name = $ref->getName();
-
-			// Skip PHP magic "__methods", but allow Primi magic "__methods__".
-			if (\str_starts_with($name, '__') && !\str_ends_with($name, '__')) {
-				continue;
-			}
-
-			$doc = $ref->getDocComment() ?: '';
-			$db = DocBlockParser::parse($doc);
-
-			$fnFlags = [];
-			if ($fnTag = $db->getTag('primi.function')) {
-
-				if ($fnTag->hasArg('inject-context')) {
-					$fnFlags[] = FnContainer::FLAG_INJECT_CONTEXT;
-				}
-
-				if ($fnTag->hasArg('no-stack')) {
-					$fnFlags[] = FnContainer::FLAG_NO_STACK;
-				}
-
-				$result[$name] = new FuncValue(
-					FnContainer::buildFromClosure([$ext, $name], $fnFlags)
-				);
-
-			}
-
-		}
-
-		return $result;
-
+		return MethodExtractor::extractMethods(new static);
 	}
 
 }
