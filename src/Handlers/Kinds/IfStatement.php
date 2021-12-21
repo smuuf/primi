4c56f858c9a17e012c9146ae40e5bfcc5847da06
Smuuf <premavansmuuf@gmail.com>
2021-12-21 19:46:45 +0100

    Implemented "elif" (else-if) capability for "if" statements. (close #29)

--- src/Handlers/Kinds/IfStatement.php
+++ src/Handlers/Kinds/IfStatement.php
@@ -16,15 +16,46 @@ class IfStatement extends SimpleHandler {
 	protected static function handle(array $node, Context $context) {
 
 		// Execute the left-hand node and get its return value.
-		$result = HandlerFactory::runNode($node['left'], $context);
+		$result = HandlerFactory::runNode($node['cond'], $context);
 
 		// If the result of the left hand equals to truthy value,
 		// execute the code branch stored in the right-hand node.
 		if ($result->isTruthy()) {
-			HandlerFactory::runNode($node['right'], $context);
-		} elseif (isset($node['else'])) {
-			HandlerFactory::runNode($node['else'], $context);
+			HandlerFactory::runNode($node['block'], $context);
+			return;
 		}
+
+		// If there are any elifs, go through each one of them and if condition
+		// of any them evaluates as truthy, run their block (but only the first
+		// elif with the truthy condition).
+		foreach ($node['elifs'] as $elif) {
+			$result = HandlerFactory::runNode($elif['cond'], $context);
+			if ($result->isTruthy()) {
+				HandlerFactory::runNode($elif['block'], $context);
+				return;
+			}
+		}
+
+		// Check existence of "else" block and execute it, if it's present.
+		if (isset($node['elseBlock'])) {
+			HandlerFactory::runNode($node['elseBlock'], $context);
+		}
+
+	}
+
+	public static function reduce(array &$node): void {
+
+		$elifs = [];
+		foreach ($node['elifCond'] ?? [] as $i => $elifCond) {
+			$elifs[] = [
+				'cond' => $elifCond,
+				'block' => $node['elifBlock'][$i],
+			];
+		}
+
+		unset($node['elifCond']);
+		unset($node['elifBlock']);
+		$node['elifs'] = $elifs;
 
 	}
 
