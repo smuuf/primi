c25b80119dfb9898696a036e17bbbff9bcc7f060
Smuuf <premavansmuuf+git@gmail.com>
2022-11-19 17:43:33 +0100

    `TerminalIoDriver` now handles retrieving multiline input from history correctly.

--- src/Drivers/TerminalIoDriver.php
+++ src/Drivers/TerminalIoDriver.php
@@ -11,8 +11,26 @@ class TerminalIoDriver implements StdIoDriverInterface {
 
 	use StrictObject;
 
+	private ?string $buffer = null;
+
+	private function bufferInput(string $buffer) {
+		$this->buffer = $buffer;
+	}
+
 	public function input(string $prompt): string {
-		return \readline($prompt);
+
+		\readline_callback_handler_install($prompt, $this->bufferInput(...));
+
+		while (true) {
+			\readline_callback_read_char();
+			if ($this->buffer !== null) {
+				$buffer = $this->buffer;
+				$this->buffer = null;
+				\readline_callback_handler_remove();
+				return $buffer;
+			}
+		}
+
 	}
 
 	public function stdout(string ...$text): void {
