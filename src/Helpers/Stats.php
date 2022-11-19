53438c5eb81685748f545db31b238f57fdb8db84
Smuuf <premavansmuuf+git@gmail.com>
2022-11-19 14:55:08 +0100

    Simplified `Stats` gatherer helper.

--- src/Helpers/Stats.php
+++ src/Helpers/Stats.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Smuuf\Primi\Helpers;
 
+use Smuuf\Primi\Cli\Term;
 use \Smuuf\StrictObject;
 
 /**
@@ -13,9 +14,6 @@ abstract class Stats {
 
 	use StrictObject;
 
-	/** @var bool If true, statistics are gathered. Disabled by default. */
-	private static $enabled = \false;
-
 	/** @var float Point in time when stats gathering was enabled. */
 	private static float $startTime = 0;
 
@@ -23,56 +21,24 @@ abstract class Stats {
 	private static array $stats = [];
 
 	/**
-	 * Enable global stats gathering.
+	 * Initialize stats gathering.
 	 */
-	public static function enable(bool $state = \true): void {
-		self::$enabled = $state;
+	public static function init(): void {
 		self::$startTime = Func::monotime();
 	}
 
 	/**
-	 * Increment counter of stats entry by 1.
-	 * The stats entry is initialized with 0 if it doesn't exist yet.
+	 * Increment counter of some stats entry by 1 (the entry is initialized
+	 * with 0 if it doesn't exist yet
 	 */
 	public static function add(string $entryName): void {
-
-		if (!self::$enabled) {
-			return;
-		}
-
 		self::$stats[$entryName] = (self::$stats[$entryName] ?? 0) + 1;
-
 	}
 
 	/**
-	 * Return an array of multiple stats entries based on some prefix.
-	 * The prefix is trimmed off the stats entry name.
-	 *
-	 * @return array<string, int>
+	 * Return a single stats entry by its name.
 	 */
-	public static function multi(string $prefix = ''): array {
-
-		$result = [];
-		$trim = \strlen($prefix);
-
-		foreach (self::$stats as $name => $value) {
-
-			if ($prefix && \strpos($name, $prefix) !== 0) {
-				continue;
-			}
-
-			$result[\substr($name, $trim)] = $value;
-
-		}
-
-		return $result;
-
-	}
-
-	/**
-	 * Return a single stats entry by its full name.
-	 */
-	public static function single(string $name): int {
+	public static function get(string $name): int {
 		return self::$stats[$name] ?? 0;
 	}
 
@@ -80,10 +46,11 @@ abstract class Stats {
 
 		self::out(Colors::get("{yellow}Runtime stats:{_}"));
 
+		//
 		// Print general stats.
+		//
 
 		self::out(Colors::get("{green}General:{_}"));
-
 		self::out("PHP: " . PHP_VERSION);
 
 		$mb = round(memory_get_peak_usage(true) / 1e6, 2);
@@ -92,19 +59,22 @@ abstract class Stats {
 		$duration = round(Func::monotime() - self::$startTime, 2);
 		self::out("Runtime duration: {$duration} s");
 
-		// Print gathered stats.
+		//
+		// Print gathered stats, if there are any.
+		//
 
-		self::out(Colors::get("{green}Gathered:{_}"));
-
-		ksort(self::$stats);
-		foreach (self::$stats as $name => $value) {
-			self::out("- {$name}: {$value}");
+		if (self::$stats) {
+			self::out(Colors::get("{green}Gathered:{_}"));
+			ksort(self::$stats);
+			foreach (self::$stats as $name => $value) {
+				self::out("- {$name}: {$value}");
+			}
 		}
 
 	}
 
 	private static function out(string $text): void {
-		echo "$text\n";
+		Term::stderr(Term::line($text));
 	}
 
 }
--- tests/unit/stats.phpt
+++ tests/unit/stats.phpt
@@ -10,52 +10,20 @@ require __DIR__ . '/../bootstrap.php';
 // Stats gatherer.
 //
 
-//
-// Stats gathering is disabled by default.
-//
-
 Stats::add('prefix_a_something');
-Assert::type('array', Stats::multi('prefix_a_'), 'Stats::multi() returns an array - even if empty');
+
 Assert::falsey(
-	Stats::multi('prefix_a_'),
+	Stats::get('prefix_a_something'),
 	"AbstractValue for 'prefix_a_something' is missing - because it was not incremented - because of disabled stats gathering"
 );
 Assert::falsey(
-	Stats::single('prefix_a_something'),
-	"AbstractValue for 'prefix_a_something' is missing - because it was not incremented - because of disabled stats gathering"
-);
-Assert::falsey(
-	Stats::single('prefix_a_nonexistent'),
+	Stats::get('prefix_a_nonexistent'),
 	"AbstractValue for 'prefix_a_nonexistent' is missing because of disabled stats gathering"
 );
 
-//
-// Enable gathering stats.
-//
-
-Stats::enable();
 Stats::add('prefix_a_something'); // Create stats entry and increment its counter by 1.
-
-Assert::type('array', Stats::multi('prefix_a_'), 'Stats::multi() returns an array.');
-Assert::same(
-	['something' => 1],
-	Stats::multi('prefix_a_'),
-	"AbstractValue for 'prefix_a_something' is present and has prefix trimmed off."
-);
-
 Stats::add('prefix_a_something'); // Increment existing stats entry by 1.
 Stats::add('prefix_b_something'); // Create a new stats entry and increment its counter by 1.
 
-Assert::same(
-	['something' => 2],
-	Stats::multi('prefix_a_'),
-	"AbstractValue for 'prefix_a_something' is present and has correct value."
-);
-Assert::same(
-	['something' => 1],
-	Stats::multi('prefix_b_'),
-	"AbstractValue for 'prefix_b_something' is present and has correct value."
-);
-
-Assert::same(2, Stats::single('prefix_a_something'), "Getting value for a single stats entry");
-Assert::same(1, Stats::single('prefix_b_something'), "Getting value for a single stats entry");
+Assert::same(2, Stats::get('prefix_a_something'), "Getting value for a get stats entry");
+Assert::same(1, Stats::get('prefix_b_something'), "Getting value for a get stats entry");
